2722087b58b2f1a011f68931a8ef4e984f447529
andreik <gs1994@gmail.com>
2024-01-02 01:41:52 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
+const http = require('http');
 const https = require('https');
-const express = require('express');
-const cookieParser = require('cookie-parser');
-//const bodyParser = require("body-parser");
 const fs = require('fs');
 const path = require('path');
 
@@ -21,8 +19,6 @@ const log = (data) => {
 
 const getLog = () => logData.reverse().join('\n\n');
 
-const clearLog = () => logData.splice(0, logData.length);
-
 const cert = fs.readFileSync(path.resolve(process.cwd(), 'ssl/gdmn.app.crt'));
 const key = fs.readFileSync(path.resolve(process.cwd(), 'ssl/gdmn.app.key'));
 
@@ -38,96 +34,48 @@ if (!ca) {
   log(`CA file is valid. Number of certificates: ${ca.length}...`);
 }
 
-const app = express();
-app.use(express.json()); // for parsing application/json
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(cookieParser());
-//app.use(bodyParser.json());
-
-const port = 443;
-
 const hosts = {
-  'coder-ai.gdmn.app': 'http://localhost:3001',
+  'coder-ai.gdmn.app': {
+    host: 'localhost',
+    port: 3001
+  },
 };
 
-const stat = {
+const app = (req, res) => {
+  if (req.url === '/_reverse_proxy_log' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write(getLog());
+    res.end();
+  } else {
+    const redirectTo = hosts[req.headers?.host];
 
-};
+    if (redirectTo) {
+      log(`${req.headers.host}${req.url}`);
 
-app.get('/_reverse_proxy_about', (_req, res) => {
-  res
-    .set('Content-Type', 'text/plain')
-    .send('Reverse Proxy Server');
-});
-
-app.get('/_reverse_proxy_log', (_req, res) => {
-  res
-    .set('Content-Type', 'text/plain')
-    .send(getLog());
-});
-
-app.get('/_reverse_proxy_stat', (_req, res) => {
-  res
-    .set('Content-Type', 'text/plain')
-    .send(JSON.stringify(stat));
-});
-
-app.all('*', async (req, res) => {
-  const fullOriginalUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-  const host = req.get('host');
-
-  if (!host) {
-    log(`>>> server error: no host in request`);
-    res.status(500).send('Internal Server Error');
-    return;
-  }
-
-  let redirectToHost = hosts[host];
-
-  if (!redirectToHost) {
-    if (host.substring(0, 4).toLowerCase() === 'www.') {
-      redirectToHost = hosts[host.substring(4)];
-    }
-  }
-
-  if (redirectToHost) {
-    try {
-      const fullRedirectUrl = redirectToHost + req.originalUrl;
-
-      log(`>>> ${req.method} ${fullOriginalUrl} --> ${fullRedirectUrl}`);
-      log(`>>> ${JSON.stringify(req.headers, null, 2)}`);
-      const response = await fetch(fullRedirectUrl, {
+      const http_client = http.request({
+        host: redirectTo.host,
+        port: redirectTo.port,
+        path: req.url,
         method: req.method,
         headers: req.headers,
-        body: (req.method === 'GET' || req.method === 'HEAD') ? undefined : JSON.stringify(req.body),
+        body: req.body
+      }, (resp) => {
+        res.writeHead(resp.statusCode, resp.headers);
+        resp.pipe(res);
       });
 
-      const headers = Array.from(response.headers)
-        // Be careful about content-encoding header!
-        .filter(([key]) => !key.includes('content-encoding'))
-        .reduce((headers, [key, value]) => ({ [key]: value, ...headers }), {});
+      req.pipe(http_client);
+    } else {
+      log(`Not found: ${req.headers.host}, ${req.url}`);
 
-      const body = await response.text();
-
-      res.set(headers).status(response.status).send(body);
-
-      log(`>>> ${response.status} ${response.statusText}`);
-    } catch (error) {
-      log(`>>> server error: ${error}`);
-      res.status(500).send('Internal Server Error');
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.write('Not Found');
+      res.end();
     }
-  } else {
-    log(`>>> ${req.method} ${fullOriginalUrl} --> 404`);
-    res.status(404).send('Server Not Found');
   }
-});
+};
 
-const httpsServer = https.createServer({ cert, ca, key }, app);
+https
+  .createServer({ cert, ca, key }, app)
+  .listen(443, () => log(`>>> HTTPS server is running at https://localhost`));
 
-httpsServer.listen(port, () => log(`>>> HTTPS server is running at https://localhost:${port}`));
-
-/*
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
-*/
